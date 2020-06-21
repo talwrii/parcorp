e2d526244e529c6e36025493f748dd9b14537a77
Tom Wright <tom.wright@genieai.co>
2020-06-21 22:39:47 +0100

    Various exploration tools used when I actually revived this tool.

--- parcorp/parcorp.py
+++ parcorp/parcorp.py
@@ -1,13 +1,38 @@
 import argparse
-import time
-import os
+import collections
 import itertools
-from lxml.etree import tostring
 import logging
 import os
+import time
+from typing import List
+
+import lxml.etree
 import sqlite3
 
+
 LOGGER = logging.getLogger()
+
+
+def filter_options(parser):
+    parser.add_argument(
+        "--or",
+        "-o",
+        type=str,
+        help="Search for any of these strings",
+        action="append",
+        dest="additional_terms",
+    )
+    parser.add_argument(
+        "--exact",
+        "-x",
+        action="store_true",
+        default=[],
+        help="search for exact string",
+    )
+
+
+def csv(s: str) -> List[str]:
+    return s.split(",")
 
 
 PARSER = argparse.ArgumentParser(description="")
@@ -19,26 +44,44 @@ PARSER.add_argument(
 )
 
 
-search_parser = parsers.add_parser("drop", help="Delete the database")
+drop_parser = parsers.add_parser("drop", help="Delete the database.")
+
+drop_parser = parsers.add_parser("info", help="Display info about the database.")
+
+syn_parser = parsers.add_parser(
+    "synonyms", help="Try to work out mappings between words"
+)
+syn_parser.add_argument(
+    "target_csv", type=csv, help="Find sources that contain one of these words"
+)
+syn_parser.add_argument(
+    "source_csv",
+    type=csv,
+    nargs="?",
+    help="Find targets that contain one of these words.",
+    default=[],
+)
+
 
 search_parser = parsers.add_parser("search", help="Search the database")
 search_parser.add_argument(
     "term", type=str, nargs="*", help="Search strings (SQLITE full text search)"
 )
+display_limit = search_parser.add_mutually_exclusive_group()
+display_limit.add_argument(
+    "-S", "--only-source", action="store_true", help="Only show the source results"
+)
+display_limit.add_argument(
+    "-T", "--only-target", action="store_true", help="Only show the target results"
+)
+
 search_parser.add_argument(
     "--skip", type=int, metavar="SKIP", help="Output one out of SKIP results"
 )
-search_parser.add_argument(
-    "--or",
-    "-o",
-    type=str,
-    help="Search for any of these strings",
-    action="append",
-    dest="additional_terms",
-)
-search_parser.add_argument(
-    "--exact", "-x", action="store_true", default=False, help="search for exact string"
-)
+search_parser.add_argument("--words", "-w", action="store_true")
+filter_options(search_parser)
+
+
 search_parser.add_argument(
     "--count",
     "-c",
@@ -47,7 +90,14 @@ search_parser.add_argument(
     help="Output the number of results",
 )
 search_parser.add_argument(
-    "--words", "-w", type=int, help="Return searches with this many words"
+    "--group",
+    "-g",
+    type=str,
+    action="append",
+    help="Count the number of results that contain this.",
+)
+search_parser.add_argument(
+    "--max", "-m", type=int, help="Return searches with this many words"
 )
 
 
@@ -75,6 +125,84 @@ data_dir = os.path.join(os.environ["HOME"], ".parcorp")
 filename = os.path.join(data_dir, "data.sql")
 
 
+def load(args):
+    with open(args.en) as english_stream:
+        with open(args.de) as german_stream:
+            os.unlink(filename)
+            connection = sqlite3.connect(filename)
+            create_table(connection)
+
+            for index, (english, german) in enumerate(
+                zip(english_stream, german_stream)
+            ):
+                if args.limit is not None and index > args.limit:
+                    break
+
+                if index % 1000 == 0:
+                    print(index)
+
+                sql_insert_pair(connection, english, german)
+
+            connection.commit()
+
+
+def load_tmx(args):
+    # pylint: disable=too-many-branches
+
+    TU, VALUE1, VALUE2 = "tu", "tu1", "tu2"
+
+    LOGGER.debug("Opening %r", filename)
+    connection = sqlite3.connect(filename)
+    create_table(connection)
+
+    with open(args.file, "br") as stream:
+        state = None
+
+        string1 = string2 = ""
+        index = 0
+        start = time.time()
+        for event, element in lxml.etree.iterparse(
+            stream, events=("start", "end")
+        ):  # pylint: disable=c-extension-no-member
+            if event == "start":
+                if element.tag == "tu":
+                    string1 = string2 = ""
+                    state = TU
+                elif element.tag == "tuv":
+                    if state == TU:
+                        state = VALUE1
+                    elif state == VALUE1:
+                        state = VALUE2
+                    else:
+                        raise ValueError(state)
+            else:
+                if element.tag == "seg":
+                    if state == VALUE1:
+                        string1 += element.text
+                    elif state == VALUE2:
+                        string2 += element.text
+                    else:
+                        raise ValueError(state)
+                elif element.tag == "tu":
+                    sql_insert_pair(connection, string2, string1)
+                    index += 1
+                    if args.limit and index > args.limit:
+                        break
+                    if index % 1000 == 0:
+                        taken = time.time() - start
+                        print(
+                            "\r{} items inserted in {:.1f} seconds. {:.1f} item/s".format(
+                                index, taken, index * 1.0 / taken
+                            )
+                        )
+                elif element.tag in ("header", "tuv"):
+                    continue
+                else:
+                    raise ValueError(element.tag)
+
+    connection.commit()
+
+
 def main():
     args = PARSER.parse_args()
     if args.debug:
@@ -82,131 +210,153 @@ def main():
 
     if not os.path.isdir(data_dir):
         os.mkdir(data_dir)
-
-    if args.command == "load":
-        with open(args.en) as english_stream:
-            with open(args.de) as german_stream:
-                os.unlink(filename)
-                connection = sqlite3.connect(filename)
-                create_table(connection)
-
-                for index, (english, german) in enumerate(
-                    zip(english_stream, german_stream)
-                ):
-                    if args.limit is not None and index > args.limit:
-                        break
-
-                    if index % 1000 == 0:
-                        print(index)
-
-                    sql_insert_pair(connection, english, german)
-
-                connection.commit()
-    if args.command == "loadtmx":
-        import lxml.etree
-
-        TU, VALUE1, VALUE2 = "tu", "tu1", "tu2"
-
-        LOGGER.debug("Opening %r", filename)
-        connection = sqlite3.connect(filename)
-        create_table(connection)
-
-        with open(args.file, "br") as stream:
-            state = None
-
-            string1 = string2 = ""
-            index = 0
-            start = time.time()
-            for event, element in lxml.etree.iterparse(stream, events=("start", "end")):
-                if event == "start":
-                    if element.tag == "tu":
-                        string1 = string2 = ""
-                        state = TU
-                    elif element.tag == "tuv":
-                        if state == TU:
-                            state = VALUE1
-                        elif state == VALUE1:
-                            state = VALUE2
-                        else:
-                            raise ValueError(state)
-
-                    continue
-                else:
-                    if element.tag == "seg":
-                        if state == VALUE1:
-                            string1 += element.text
-                        elif state == VALUE2:
-                            string2 += element.text
-                        else:
-                            raise ValueError(state)
-                    elif element.tag == "tu":
-                        sql_insert_pair(connection, string2, string1)
-                        index += 1
-                        if args.limit and index > args.limit:
-                            break
-                        if index % 1000 == 0:
-                            taken = time.time() - start
-                            print(
-                                "\r{} items inserted in {:.1f} seconds. {:.1f} item/s".format(
-                                    index, taken, index * 1.0 / taken
-                                )
-                            ),
-                    elif element.tag in ("header", "tuv"):
-                        continue
-                    else:
-                        raise ValueError(element.tag)
-
-        connection.commit()
+    elif args.command == "load":
+        load(args)
+    elif args.command == "loadtmx":
+        load_tmx(args)
     elif args.command == "drop":
         os.unlink(filename)
-    elif args.command == "search":
-        args.additional_terms = args.additional_terms or []
+    elif args.command == "info":
         connection = sqlite3.connect(filename)
         cursor = connection.cursor()
-        term = " ".join(args.term)
-        if args.exact:
-            term = '"{}"'.format(term)
+        cursor.execute("select count(*) from translation")
+        print(cursor.fetchall())
+    elif args.command == "search":
+        search(args)
+    elif args.command == "synonyms":
+        synonyms(args)
+    else:
+        raise ValueError(args.command)
 
-        if args.target or args.source:
-            language_field = "german" if args.target else "english"
-            condition = " or ".join(
-                ["{} MATCH ?".format(language_field)]
-                * (1 + len(args.additional_terms or []))
-            )
-            terms = (term.decode("utf8"),) + tuple(
-                x.decode("utf8") for x in args.additional_terms
-            )
-        else:
-            condition = " or ".join(
-                ["german MATCH ? or english MATCH ?"]
-                * (1 + len(args.additional_terms or []))
-            )
-            terms = (term.decode("utf8"), term.decode("utf8")) + tuple(
-                itertools.chain.from_iterable(
-                    (x.decode("utf8"), x.decode("utf8")) for x in args.additional_terms
-                )
-            )
 
-        sql = "select german, english from translation where {} order by length(german)".format(
-            condition
+def synonyms(args):
+    "Try to find the synonyms fora word"
+    connection = sqlite3.connect(filename)
+    sql, sql_terms = build_search_sql([], args.target_csv)
+    cursor = connection.cursor()
+    LOGGER.debug("Running sql %r %r", sql, sql_terms)
+    cursor.execute(sql, sql_terms)
+
+    counts = collections.Counter()
+    for german, english in cursor.fetchall():
+        german = strip_punct(german)
+        english = strip_punct(english)
+        german_words = get_words(german)
+        english_words = get_words(english)
+
+        counts.update(
+            [
+                (g, e)
+                for g in german_words
+                for e in english_words
+                if e in args.target_csv
+                and (not args.source_csv or g in args.source_csv)
+            ]
         )
-        LOGGER.debug("Running sql %r".format(sql))
 
-        cursor.execute(sql, terms)
-        if args.count:
-            print(len(cursor.fetchall()))
+    for pair in sorted(counts, key=counts.get, reverse=True):
+        print(pair, counts[pair])
+
+
+def strip_punct(s: str) -> str:
+    for c in ".,!":
+        s = s.replace(c, "")
+    return s
+
+
+def get_words(s: str) -> List[str]:
+    return [w.lower() for w in s.split()]
+
+
+def build_search_sql(german_terms, english_terms):
+    def build_condition(field, terms):
+        condition_template = " or ".join(["{} MATCH ?".format(field)] * len(terms))
+        wrapped_condition = "({})".format(condition_template)
+
+        if terms:
+            return [wrapped_condition]
         else:
-            for index, (german, english) in enumerate(cursor.fetchall()):
-                if args.skip is not None and index % args.skip != 0:
+            return []
+
+    german_query = build_condition("german", german_terms)
+    english_query = build_condition("english", english_terms)
+
+    sql = "select german, english from translation where {} order by max(length(german), length(english))".format(
+        " or ".join(german_query + english_query)
+    )
+
+    return sql, german_terms + english_terms
+
+
+def search(args):
+    args.additional_terms = args.additional_terms or []
+    connection = sqlite3.connect(filename)
+    cursor = connection.cursor()
+    terms = [" ".join(args.term)] + (args.additional_terms or [])
+
+    if args.exact:
+        terms = ['"{}"'.format(t) for f in terms]
+
+    if args.target:
+        german_terms = terms[:]
+        english_terms = []
+    elif args.source:
+        german_terms = []
+        english_terms = terms[:]
+    else:
+        german_terms = terms[:]
+        english_terms = terms[:]
+
+    sql, sql_terms = build_search_sql(german_terms, english_terms)
+
+    LOGGER.debug("Running sql {!r}".format(sql))
+    cursor.execute(sql, sql_terms)
+
+    if args.count:
+        print(len(cursor.fetchall()))
+    else:
+        words = collections.Counter()
+        groups = collections.defaultdict(int)
+        for index, (german, english) in enumerate(cursor.fetchall()):
+            if args.skip is not None and index % args.skip != 0:
+                continue
+
+            if args.max is not None:
+                if min(len(german.split(" ")), len(english.split(" "))) < args.max:
                     continue
-                if args.words is not None:
-                    if (
-                        min(len(german.split(" ")), len(english.split(" ")))
-                        < args.words
-                    ):
-                        continue
-                print("", german, end="")
-                print("ENGLISH", english)
+
+            if args.group:
+                containing_word, *_ = itertools.chain(
+                    [w for w in args.group if w in german or w in english], [None]
+                )
+                if containing_word:
+                    groups[containing_word] += 1
+                    continue
+                else:
+                    groups["unknown"] += 1
+
+            if args.words:
+                if args.only_source:
+                    words.update(english.split())
+                elif args.only_target:
+                    words.update(german.split())
+                else:
+                    words.update(english.split())
+                    words.update(german.split())
+            else:
+                if args.only_source:
+                    print(f"{english}")
+                elif args.only_target:
+                    print(f"{german}")
+                else:
+                    print(f"{german} -> {english}")
+
+        if args.words:
+            for word in sorted(words, key=words.get, reverse=True):
+                print(word, words[word])
+        else:
+            for word, count in groups.items():
+                print(f"{word}: {count}")
 
 
 def create_table(connection):
